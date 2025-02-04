44720a97ebccb375b8cb1c20f53853223c110df4
HP Pro Mini <HP Pro Mini@HYPER-JULY>
2025-02-04 13:52:20 +0800

    Establishing Dependency Imjection Connection of Application, Domain, Infrastructure

---
+++ DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using CoffeeShop.Application;
+using CoffeeShop.Infrastructure;
+
+namespace coffee_shop
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddMainDependency(this IServiceCollection services)
+        {
+            services.AddDependencyApplication();
+            services.AddDependencyInsfrastructure();
+            return services;
+        }
+    }
+}
